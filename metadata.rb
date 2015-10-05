846f8c1a401e0ed2a29f7d230c92250a6f127215
David Brown <dmlb2000@gmail.com>
2015-10-05 15:21:26 -0700

    here's some updated bits to include vagrant and appropriate plugins

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,10 @@ maintainer_email 'dmlb2000@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures dmlb2000_chef'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 depends 'omnibus_updater'
+depends 'build-essential'
 depends 'chef-client'
 depends 'chef-dk'
+depends 'vagrant'
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,6 +6,23 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+package 'libvirt-devel'
+package 'libffi-devel'
+package 'zlib-devel'
+link '/etc/alternatives/ld' do
+  to '/usr/bin/ld.gold'
+end
+include_recipe 'build-essential'
+include_recipe 'vagrant'
+%w(
+vagrant-libvirt
+vagrant-openstack-provider
+).each do |plugin|
+  execute "/opt/vagrant/embedded/bin/gem install #{plugin} --no-user-install" do
+    environment 'SSL_CERT_FILE' => '/opt/vagrant/embedded/cacert.pem'
+    not_if "/opt/vagrant/embedded/bin/gem list --local | grep -q #{plugin}"
+  end
+end
 include_recipe 'omnibus_updater'
 include_recipe 'chef-client'
 include_recipe 'chef-dk'
