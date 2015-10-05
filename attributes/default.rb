a253b0a40cc2ca1eb07b6c70b57f8ffbc3ba3147
David Brown <dmlb2000@gmail.com>
2015-10-05 16:14:15 -0700

    this doesn't work vagrant can't see the plugins this way

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,9 @@
 default['vagrant']['version'] = '1.7.2'
 default['vagrant']['url']         = vagrant_package_uri(node['vagrant']['version'])
 default['vagrant']['checksum']    = vagrant_sha256sum(node['vagrant']['version'])
+
+default['vagrant']['plugins'] = %w(
+  vagrant-libvirt
+  vagrant-openstack-provider
+)
+default['vagrant']['user'] = 'dmlb2000'
