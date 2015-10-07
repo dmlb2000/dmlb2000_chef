b30614db5202ae53e89075b54649439b5fbe9703
David Brown <dmlb2000@gmail.com>
2015-10-07 00:21:13 -0700

    back level things

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['vagrant']['version'] = '1.7.2'
+default['vagrant']['version'] = '1.7.4'
 default['vagrant']['url']         = vagrant_package_uri(node['vagrant']['version'])
 default['vagrant']['checksum']    = vagrant_sha256sum(node['vagrant']['version'])
 
