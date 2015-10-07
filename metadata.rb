804dbedf73bb919150da8b22d06c983d23a5f271
David Brown <dmlb2000@gmail.com>
2015-10-06 22:00:30 -0700

    hard fix vagrant back one version

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,10 @@ maintainer_email 'dmlb2000@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures dmlb2000_chef'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.0'
+version          '0.3.1'
 
 depends 'omnibus_updater'
 depends 'build-essential'
 depends 'chef-client'
 depends 'chef-dk'
-depends 'vagrant'
+depends 'vagrant', '=0.3.0'
