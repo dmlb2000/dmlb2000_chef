55cf457ebed507ca4387b4677992c86007ab746f
David Brown <dmlb2000@gmail.com>
2016-01-10 22:06:08 -0800

    do some vagrant version bits

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,3 @@
-default['vagrant']['version'] = '1.7.4'
-default['vagrant']['url'] = vagrant_package_uri(node['vagrant']['version'])
-default['vagrant']['checksum'] = vagrant_sha256sum(node['vagrant']['version'])
-
 default['vagrant']['plugins'] = %w(
   vagrant-libvirt
   vagrant-openstack-provider
