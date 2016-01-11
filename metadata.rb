5c82e47561f577bba7ccae7fcffad848bb56790c
David Brown <dmlb2000@gmail.com>
2016-01-10 21:47:42 -0800

    remove depends on vagrant cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,10 @@ maintainer_email 'dmlb2000@gmail.com'
 license 'All rights reserved'
 description 'Installs/Configures dmlb2000_chef'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.3.10'
+version '0.3.11'
 
 depends 'omnibus_updater'
 depends 'build-essential'
 depends 'chef-client'
 depends 'chef-dk'
-depends 'vagrant', '=0.3.0'
+depends 'vagrant'
