c482ff6ee841ba59bb4ddf710ffa9a39c712530c
David Brown <dmlb2000@gmail.com>
2016-01-11 23:18:27 -0800

    dumb change

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dmlb2000@gmail.com'
 license 'All rights reserved'
 description 'Installs/Configures dmlb2000_chef'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.3.16'
+version '0.3.15'
 
 depends 'omnibus_updater'
 depends 'build-essential'
