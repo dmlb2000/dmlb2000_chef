a57b4bedf03bce0d464389ab262af283d3d5d92f
David Brown <dmlb2000@gmail.com>
2016-02-22 21:59:05 -0800

    version bump this

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dmlb2000@gmail.com'
 license 'All rights reserved'
 description 'Installs/Configures dmlb2000_chef'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.3.18'
+version '1.0.0'
 
 depends 'omnibus_updater'
 depends 'chef-client'
